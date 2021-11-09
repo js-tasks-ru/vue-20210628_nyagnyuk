06ebf2a48b6937fe240132ec4273fbd5fc1ba97e
vynokur@zfort.com <vynokur@zfort.com>
2021-11-09 12:31:51 +0200

    02-VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,59 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const Calc = {
+  data() {
+    return {
+      numberFirst: '',
+      numberSecond: '',
+      result: '',
+      picked: '',
+    }
+  },
+
+  methods: {
+    add(){
+      this.result = this.numberFirst + this.numberSecond;
+    },
+    subtract(){
+      this.result = this.numberFirst - this.numberSecond;
+    },
+    multiply(){
+      this.result = this.numberFirst * this.numberSecond;
+    },
+    divide(){
+      this.result = this.numberFirst / this.numberSecond;
+    },
+
+    resultCalc(){
+      if(this.picked == "sum"){
+        this.add();
+      }
+      if(this.picked == "subtract"){
+        this.subtract();
+      }
+      if(this.picked == "multiply"){
+        this.multiply();
+      }
+      if(this.picked == "divide"){
+        this.divide();
+      }
+    },
+  },
+  watch: {
+    numberFirst: {
+      immediate: true,
+      handler() {
+        this.resultCalc();
+      }
+    },
+    numberSecond: {
+      immediate: true,
+      handler() {
+        this.resultCalc();
+      }
+    },
+  }
+}
+
+const app = createApp(Calc);
+const vm = app.mount('#app');
